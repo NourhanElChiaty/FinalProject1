3bd782d2ea01dcd841dd36507a445df5e94a12c2
RoaaKhatab <RoaaKhatab@users.noreply.github.com>
2018-05-01 17:11:51 +0200

    Add project files.

---
+++ FinalProject1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalProject1.Startup))]
+namespace FinalProject1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
